91db3e60e7019eee55bda30cfb85d2adff16d3ba
Vinicius Reisch <vinicius.reisch@acesso.edu>
2022-07-15 09:46:54 -0300

    Updating classes to static method

--- code/classes.py
+++ code/classes.py
@@ -9,22 +9,24 @@ cursor = conexao.cursor()
 
 
 class Mercado:
+    @staticmethod
     def cadastrar_Cliente(self, nome, numero):
-        self.cep = input('Qual seu cep? ')
-        cep = requests.get('https://cep.awesomeapi.com.br/json/' + self.cep)
+        cep = input('Qual seu cep? ')
+        cep = requests.get('https://cep.awesomeapi.com.br/json/' + cep)
         cep_json = cep.json()
         while True:
             cpf = validar_Cpf()
             try:
                 cursor.execute('INSERT INTO clientes(nome, cpf, cep, estado, bairro, rua, numero)'
-                           'VALUES(?,?,?,?,?,?,?)',
-                           (nome, cpf, self.cep, cep_json['state'], cep_json['district'], cep_json['address_name'], numero))
+                               'VALUES(?,?,?,?,?,?,?)',
+                               (nome, cpf, cep, cep_json['state'], cep_json['district'], cep_json['address_name'],
+                                numero))
                 conexao.commit()
                 break
             except:
                 print('O cpf já foi cadastrado, cadastre outro.')
 
-
+    @staticmethod
     def editar_Cliente(self):
         x = input('Qual cliente deseja editar [CPF]: ')
         infos = [
@@ -44,22 +46,24 @@ class Mercado:
         cursor.execute(f'UPDATE clientes SET {infos[y]}="{new_info}" WHERE cpf={x}')
         conexao.commit()
 
+    @staticmethod
     def excluir_cliente(self, clienteDesejado):
         cursor.execute(f'DELETE FROM clientes WHERE cpf={clienteDesejado}')
         conexao.commit()
 
+    @staticmethod
     def cadastrar_Produto(self, nome, familia, barras):
         while True:
             try:
                 cursor.execute('INSERT INTO produtos(nome, familia, barras)'
-                           'VALUES(?,?,?)',
-                           (nome, familia, barras))
+                               'VALUES(?,?,?)',
+                               (nome, familia, barras))
                 conexao.commit()
                 break
             except:
                 print('O codigo de barras já está cadastrado')
 
-
+    @staticmethod
     def editar_Produto(self):
         x = input('Qual o ID do produto? ')
         infos = [
@@ -75,23 +79,36 @@ class Mercado:
         cursor.execute(f'UPDATE produtos SET {infos[y]}="{new_info}" WHERE id={x}')
         conexao.commit()
 
+    @staticmethod
     def excluir_Produto(self, produtoDesejado):
         cursor.execute(f'DELETE FROM produtos WHERE barras={produtoDesejado}')
         conexao.commit()
 
-    def cadastrar_Venda(self, quantidade, valor, cpf, barras):
-
+    @staticmethod
+    def cadastrar_venda(quantidade, valor, cpf, barras):
         total = float(quantidade) * float(valor)
         data = arrow.now().format('DD/MM/YYYY')
         cursor.execute('INSERT INTO vendas(quantidade, unitario, total, data, cpf, barras)'
                        'VALUES(?,?,?,?,?,?)',
-                        (quantidade, valor, total, data, cpf, barras))
+                       (quantidade, valor, total, data, cpf, barras))
         conexao.commit()
 
+    @staticmethod
     def deletar_produtos(self):
         cursor.execute('DELETE FROM produtos WHERE id>0')
         conexao.commit()
 
+    @staticmethod
+    def deletar_clientes(self):
+        cursor.execute('DELETE FROM clientes WHERE id>0')
+        conexao.commit()
+
+    @staticmethod
+    def deletar_vendas(self):
+        cursor.execute('DELETE FROM vendas WHERE total>0')
+        conexao.commit()
+
+    @staticmethod
     def salvar(self):
         cursor.close()
         conexao.close()
